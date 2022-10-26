902c21fa786a43aec96bec1f686766faa947076c
mohamed-rabeaa <mohamedrabeaa909@gmail.com>
2022-10-26 15:16:35 +0200

    design Home page

--- client-side/src/App.js
+++ client-side/src/App.js
@@ -9,7 +9,7 @@ import Layout from './pages/Layout';
 function App() {
   return (
     <BrowserRouter>
-      <div className="App">
+      <div className="App bg-gradient-to-tl from-green-400 to-sky-900">
         <Routes>
           <Route exact path="/" element={<Layout />} >
             <Route exact path="/" element={<Home />} />
--- client-side/src/pages/Home.js
+++ client-side/src/pages/Home.js
@@ -5,8 +5,19 @@ export default function Home() {
 
 	return (
 		<>
-		<Link to='signup'>SignUp</Link>
-		<Link to='login'>Login</Link>
+			<div className='w-full min-h-screen flex items-center justify-center'>
+				<div className='bg-transparent border-2 border-violet-800 rounded-md p-16'>
+					<div>
+						<h1 className='text-4xl font-bold text-center'>Hi mohamed</h1>
+						<p className='text-slate-700 mt-4 text-center text-lg'>login is success</p>
+					</div>
+					<div className='mt-14'>
+						<Link to='signup' className='bg-blue-900 rounded-md px-12 py-4 mx-4 text-slate-300'>SignUp</Link>
+						<Link to='login' className='bg-blue-900 rounded-md px-12 py-4 mx-4 text-slate-300'>Login</Link>
+					</div>
+				</div>
+
+			</div>
 		</>
 	)
 }
